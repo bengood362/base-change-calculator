2e0089ae5ae35ee1e4d2a8260a07d5cf60dc0242
bengood362 <bengood362@gmail.com>
2017-09-26 16:38:58 +0800

    now it can be imported for other usage

--- base-change-calculator.py
+++ base-change-calculator.py
@@ -1,5 +1,6 @@
+# flags for user to customize
 base_code = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-dp = 4
+dp = 10
 esp = 1.0/(10**dp)
 # NOTE: invalidity of number & base are not checked, base 2 might have 23467890 etc
 # NOTE: Input does not accept HEX currently
@@ -37,93 +38,98 @@ def get_symbol(number): # 10->"A", 11->"B"
     return base_code[int(number)]
 
 def get_value(symbol):
-    if(number == '.'):
+    if(symbol == '.'):
         return '.'
     return base_code.index(symbol)
 
-while(1):
-    number=raw_input("number? ")
-    if(number.strip() == ''):
-        print "bye"
-        break
-    base_from=raw_input("base from? ")
-    if(base_from.strip() == ''):
-        print "bye"
-        break
-    base_to=raw_input("base to? ")
-    if(base_to.strip() == ''):
-        print "bye"
-        break
-    if(validate_base(base_from, base_to)):
-        print "incorrect base format"
-    elif(validate_number(number)):
-        print "incorrect number format"
-    else: # there is no problem for the format of input
-        # get information of the number string
-        number_of_dot = number.count('.') # actually will be 0 or 1
-        number_splitted = number.split('.')
-        digit_length_splitted_by_dot = map(lambda x: len(x), number.split('.'))
-        total_digit_length = reduce(lambda x,y: x+y,digit_length_splitted_by_dot)
-        endIndex = 0
-        if (len(digit_length_splitted_by_dot) == 2):
-            endIndex = -digit_length_splitted_by_dot[1]
-        else:
+def main():
+    while(1):
+        number=raw_input("number? ")
+        if(number.strip() == ''):
+            print "bye"
+            break
+        base_from=raw_input("base from? ")
+        if(base_from.strip() == ''):
+            print "bye"
+            break
+        base_to=raw_input("base to? ")
+        if(base_to.strip() == ''):
+            print "bye"
+            break
+        if(validate_base(base_from, base_to)):
+            print "incorrect base format"
+        elif(validate_number(number)):
+            print "incorrect number format"
+        else: # there is no problem for the format of input
+            # get information of the number string
+            number_of_dot = number.count('.') # actually will be 0 or 1
+            number_splitted = number.split('.')
+            digit_length_splitted_by_dot = map(lambda x: len(x), number.split('.'))
+            total_digit_length = reduce(lambda x,y: x+y,digit_length_splitted_by_dot)
             endIndex = 0
-        index = 0
-        step_1 = ''
-        print(number+" in base-"+base_from)
-        # step 1: decompose the number into base 10 first
-        step_1_answer = 0
-        if(number_of_dot == 0):
-            for digit in number_splitted[0]:
-                digit = get_value(digit)
-                index += 1
-                if(index == total_digit_length):
-                    step_1 += str(digit)+' * '+base_from+'^('+str(total_digit_length-index)+')'
-                    step_1_answer += int(digit)*(int(base_from)**(total_digit_length-index))
-                else:
-                    step_1 += str(digit)+' * '+base_from+'^('+str(total_digit_length-index)+') + '
-                    step_1_answer += int(digit)*(int(base_from)**(total_digit_length-index))
-        else:
-            # before dot
-            for digit in number_splitted[0]:
-                digit = get_value(digit)
-                index += 1
-                step_1 += str(digit)+' * '+base_from+'^('+str(digit_length_splitted_by_dot[0]-index)+') + '
-                step_1_answer += int(digit)*(int(base_from)**(digit_length_splitted_by_dot[0]-index))
-            # after dot
+            if (len(digit_length_splitted_by_dot) == 2):
+                endIndex = -digit_length_splitted_by_dot[1]
+            else:
+                endIndex = 0
             index = 0
-            for digit in number_splitted[1]:
-                digit = get_value(digit)
+            step_1 = ''
+            print(number+" in base-"+base_from)
+            # step 1: decompose the number into base 10 first
+            step_1_answer = 0
+            if(number_of_dot == 0):
+                for digit in number_splitted[0]:
+                    digit = get_value(digit)
+                    index += 1
+                    if(index == total_digit_length):
+                        step_1 += str(digit)+' * '+base_from+'^('+str(total_digit_length-index)+')'
+                        step_1_answer += int(digit)*(int(base_from)**(total_digit_length-index))
+                    else:
+                        step_1 += str(digit)+' * '+base_from+'^('+str(total_digit_length-index)+') + '
+                        step_1_answer += int(digit)*(int(base_from)**(total_digit_length-index))
+            else:
+                # before dot
+                for digit in number_splitted[0]:
+                    digit = get_value(digit)
+                    index += 1
+                    step_1 += str(digit)+' * '+base_from+'^('+str(digit_length_splitted_by_dot[0]-index)+') + '
+                    step_1_answer += int(digit)*(int(base_from)**(digit_length_splitted_by_dot[0]-index))
+                # after dot
+                index = 0
+                for digit in number_splitted[1]:
+                    digit = get_value(digit)
+                    index -= 1
+                    if(index == -digit_length_splitted_by_dot[1]):
+                        step_1 += str(digit)+' * '+base_from+'^('+str(index)+')'
+                        step_1_answer += int(digit)*(int(base_from)**(index))
+                    else:
+                        step_1 += str(digit)+' * '+base_from+'^('+str(index)+') + '
+                        step_1_answer += int(digit)*(int(base_from)**(index))
+            print('= '+step_1)
+            print('= '+str(step_1_answer)+" in base-10")
+            # step 2: reconstruct the number in base_to
+            # NOTE: currently is incorrect for decimal
+            step_2 = '' # if base > 10, then will need String
+            step_2_answer = []
+            index = biggest_index(int(base_to), step_1_answer)
+            while(1):
+                # step_2_answer/(int(base_to)**index)
+                # print (step_1_answer, base_to, index)
+                coeff = int(step_1_answer/(int(base_to)**index))
+                # FIXME: if coeff == 0 don't add to step
+                step_2 += str(coeff)+" * "+base_to+"^("+str(index)+") + "
+                if(index == -1):
+                    step_2_answer.append('.')
+                step_2_answer.append(coeff)
+                step_1_answer -= coeff*int(base_to)**index
                 index -= 1
-                if(index == -digit_length_splitted_by_dot[1]):
-                    step_1 += str(digit)+' * '+base_from+'^('+str(index)+')'
-                    step_1_answer += int(digit)*(int(base_from)**(index))
-                else:
-                    step_1 += str(digit)+' * '+base_from+'^('+str(index)+') + '
-                    step_1_answer += int(digit)*(int(base_from)**(index))
-        print('= '+step_1)
-        print('= '+str(step_1_answer)+" in base-10")
-        # step 2: reconstruct the number in base_to
-        # NOTE: currently is incorrect for decimal
-        step_2 = '' # if base > 10, then will need String
-        step_2_answer = []
-        index = biggest_index(int(base_to), step_1_answer)
-        while(1):
-            # step_2_answer/(int(base_to)**index)
-            # print (step_1_answer, base_to, index)
-            coeff = int(step_1_answer/(int(base_to)**index))
-            # FIXME: if coeff == 0 don't add to step
-            step_2 += str(coeff)+" * "+base_to+"^("+str(index)+") + "
-            if(index == -1):
-                step_2_answer.append('.')
-            step_2_answer.append(coeff)
-            step_1_answer -= coeff*int(base_to)**index
-            index -= 1
-            if(step_1_answer <= esp and (index<endIndex or index<=-3)):
-                step_2 = step_2[:-3]
-                break
-        print('= '+step_2)
-        print('= '+''.join(map(get_symbol,step_2_answer))+' in base-'+base_to)
-        print ''
-        # step 3: show result
+                if(step_1_answer <= esp and (index<endIndex or index<=-3)):
+                    step_2 = step_2[:-3]
+                    break
+            # step 3: show result
+            print('= '+step_2)
+            print('= '+''.join(map(get_symbol,step_2_answer))+' in base-'+base_to)
+            print ''
+
+
+if __name__ == "__main__":
+    main()
